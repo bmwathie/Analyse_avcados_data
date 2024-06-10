e28188c8c9d245f9d78ce835b3c211bfa06eed44
bmwathie <61660930+bmwathie@users.noreply.github.com>
2024-06-10 22:11:15 +0000

    Environnement big data

---
+++ docker_volume/partage_nifi/partage/xml_spark/pyspark_avocado.py
@@ -0,0 +1,37 @@
+# Importation 
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, date_format
+
+# Ouverture d'une session
+spark = SparkSession.builder.appName("AvocadoToElastic").getOrCreate()
+
+# Lire le fichier CSV
+df = spark.read.csv(f'/projet_elasticsearch_kibana/raw_avocado/avocado_5.csv', header=True, inferSchema=True)
+df.show()
+
+# Ajouter les colonnes 'jour' et 'mois'
+df = df.withColumn('jour', date_format(col('Date'), 'd'))
+df = df.withColumn('mois', date_format(col('Date'), 'M'))
+
+# Écrire le résultat dans un fichier CSV avec le numéro du fichier
+df.write.csv(r'/projet_elasticsearch_kibana/stagging_avocado/avocado_cleaned_5.csv', header=True)
+
+# Chargement pour indexer
+df_index = spark.read.option("header", True) \
+.option("inferSchema" , "true").csv(f"/projet_elasticsearch_kibana/stagging_avocado/avocado_cleaned_5.csv")
+
+# Indexer les données dans ElasticSearch
+df_index.write.format("org.elasticsearch.spark.sql") \
+.option("es.net.http.auth.user", "groupe10") \
+.option("es.net.http.auth.pass", "groupe10") \
+.option("es.nodes", "http://elasticsearch:9200") \
+.option("es.nodes.discovery", "false") \
+.option("es.nodes.wan.only", "true") \
+.option("es.index.auto.create", "true") \
+.mode("append") \
+.save("avocado")
+
+# Afficher un message pour indiquer que le traitement est terminé
+print("Traitement terminé pour tous les fichiers !")
+
+spark.stop()
